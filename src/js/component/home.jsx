9ba568e369a0b86703d8139db2462d994b5037f8
Franz Seidel <franz8818@gmail.com>
2022-09-07 13:29:33 +0000

    css-style

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,15 +6,17 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = (props) => {
 	return (
-		<div className="row container">
+<div className="card">
+	<div className="row container">
 			<div className="col-1"> <i class="far fa-clock"></i> </div>
-			<div className="col-1">{props.hour1}:</div>
-			<div className="col-1">{props.hour0} : </div>
-			<div className="col-1">{props.minute1} : </div>
-			<div className="col-1">{props.minute0} : </div>
-			<div className="col-1">{props.second1} : </div>
-			<div className="col-1">{props.second0} :</div>
-		</div>
+			<div className="col-1">{props.hour1}</div>
+			<div className="col-1">{props.hour0}</div>
+			<div className="col-1">{props.minute1}</div>
+			<div className="col-1">{props.minute0}</div>
+			<div className="col-1">{props.second1}</div>
+			<div className="col-1">{props.second0}</div>
+	</div>
+</div>
 		
 	);
 };
